aac810d5adb9cc34193c9b39fd1783c43b165002
xs170202103088 <49663043+xs170202103088@users.noreply.github.com>
2019-05-21 16:54:56 +0800

    Add files via upload

---
+++ demoProj/src/demoProj/jiaowu.java
@@ -0,0 +1,7 @@
+       package demoProj;
+			public class jiaowu {
+				public static void main(String[] args) {
+					System.out.println("good!");
+				}
+}
+
---
+++ demoProj/src/demoProj/student.java
@@ -0,0 +1,10 @@
+package demoProj;
+public class student {
+   public String name;
+   public String address;
+   
+   student(){
+	   
+   }
+}
+
